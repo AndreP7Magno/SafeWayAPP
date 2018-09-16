79f37ca2011ddacbc3ac45b35b98244a554d59c8
AndreP7Magno <andremagno14@gmail.com>
2018-09-16 18:22:14 -0300

    removendo androidTest e company name

--- app/src/main/java/Activity/MainActivity.java
+++ app/src/main/java/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.pichau.safewayapp;
+package Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.pichau.safewayapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
