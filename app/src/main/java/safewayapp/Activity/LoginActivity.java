e88b4e61833a341f7fb18d45499ed31b63cd57da
AndreP7Magno <andremagno14@gmail.com>
2018-09-18 00:05:01 -0300

    Caminho entre login e cadastro inicial

--- app/src/main/java/safewayapp/Activity/LoginActivity.java
+++ app/src/main/java/safewayapp/Activity/LoginActivity.java
@@ -1,15 +1,55 @@
 package safewayapp.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import safewayapp.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final int REQUEST_CADASTRO = 0;
+
+    @BindView(R.id.btn_login)
+    Button _loginButton;
+
+    @BindView(R.id.link_cadastro)
+    TextView _cadastroLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        pedirPermissoes();
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        _cadastroLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivityForResult(intent, REQUEST_CADASTRO);
+                finish();
+            }
+        });
+    }
+
+    private void login() {
+
+    }
+
+    private void pedirPermissoes() {
+
     }
 }
--- app/src/main/java/safewayapp/Activity/SignUpActivity.java
+++ app/src/main/java/safewayapp/Activity/SignUpActivity.java
@@ -1,15 +1,35 @@
 package safewayapp.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import safewayapp.R;
 
 public class SignUpActivity extends AppCompatActivity {
+    private static final int REQUEST_LOGIN = 0;
+
+    @BindView(R.id.link_login)
+    TextView _loginLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivityForResult(intent, REQUEST_LOGIN);
+                finish();
+            }
+        });
+
     }
 }
