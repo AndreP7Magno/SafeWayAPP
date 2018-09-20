0eaf48d45a9920bf295b9202cf97676f4e233c34
AndreP7Magno <andremagno14@gmail.com>
2018-09-20 12:57:41 -0300

    Bottom Menu e fragments criadas.

--- app/src/main/java/safewayapp/Activity/MainActivity.java
+++ app/src/main/java/safewayapp/Activity/MainActivity.java
@@ -1,13 +1,25 @@
 package safewayapp.Activity;
 
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.eightbitlab.bottomnavigationbar.BottomBarItem;
+import com.eightbitlab.bottomnavigationbar.BottomNavigationBar;
+
+import Fragment.HomeFragment;
+import Fragment.ContactFragment;
+import Fragment.ChatFragment;
+import Fragment.ProfileFragment;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import safewayapp.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_bar)
+    BottomNavigationBar bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +29,55 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
+
+        BottomBarItem home = new BottomBarItem(R.drawable.home, R.string.homeMenuBottom);
+        bottomBar.addTab(home);
+
+        BottomBarItem contact = new BottomBarItem(R.drawable.contact, R.string.contactMenuBottom);
+        bottomBar.addTab(contact);
+
+        BottomBarItem chat = new BottomBarItem(R.drawable.chat, R.string.chaMenuBottom);
+        bottomBar.addTab(chat);
+
+        BottomBarItem profile = new BottomBarItem(R.drawable.profile, R.string.profileMenuBottom);
+        bottomBar.addTab(profile);
+
+        loadFragment(new HomeFragment());
+
+        bottomBar.setOnSelectListener(new BottomNavigationBar.OnSelectListener() {
+            Fragment fragment = null;
+            @Override
+            public void onSelect(int position) {
+                switch (position){
+                    case 0:
+                        fragment = new HomeFragment();
+                        break;
+                    case 1:
+                        fragment = new ContactFragment();
+                        break;
+                    case 2:
+                        fragment = new ChatFragment();
+                        break;
+                    case 3:
+                        fragment = new ProfileFragment();
+                        break;
+                    default:
+                        fragment = new HomeFragment();
+                        break;
+                }
+
+                loadFragment(fragment);
+            }
+        });
     }
+
+    private void loadFragment(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
 }
