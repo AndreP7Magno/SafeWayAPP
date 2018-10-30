fa73342cfef7cc3070986d56263626080f4ac041
AndreP7Magno <andremagno14@gmail.com>
2018-10-29 21:54:24 -0300

    Mudanças na tela de login e cadastro - Layout, Validações, Permissões, Ícones

--- app/src/main/java/safewayapp/Activity/LoginActivity.java
+++ app/src/main/java/safewayapp/Activity/LoginActivity.java
@@ -1,19 +1,37 @@
 package safewayapp.Activity;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import safewayapp.Helper.ValidateCPFHelper;
 import safewayapp.R;
+
 
 public class LoginActivity extends AppCompatActivity {
     private static final int REQUEST_CADASTRO = 0;
+
+    @BindView(R.id.txtCPFLogin)
+    EditText _cpfText;
+
+    @BindView(R.id.txtSenhaLogin)
+    EditText _senhaText;
 
     @BindView(R.id.btn_login)
     Button _loginButton;
@@ -27,12 +45,32 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.btn_facebook)
     Button _facebookButton;
 
+    PermissionListener permissionlistener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            //Toast.makeText(LoginActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPermissionDenied(List<String> deniedPermissions) {
+            //Toast.makeText(LoginActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         pedirPermissoes();
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("Se você recusar a permissão, você não poderá usar este serviço\n\nPor favor ative as permissões em [Configuração] > [Permissão]")
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET)
+                .setDeniedCloseButtonText("Fechar")
+                .setGotoSettingButtonText("Configuração")
+                .check();
 
         _loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,12 +80,10 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         _cadastroButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
-                startActivityForResult(intent, REQUEST_CADASTRO);
-                finish();
+                startActivity(intent);
             }
         });
 
@@ -67,15 +103,46 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void login() {
+        _loginButton.setEnabled(false);
+        _cadastroButton.setEnabled(false);
 
-        //Validações
+        if(!validaCampos()){
+            _loginButton.setEnabled(true);
+            _cadastroButton.setEnabled(true);
+            Toast.makeText(getApplicationContext(), "Dados de preenchimento inválidos.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Lógica do login de usuário
 
         Intent main = new Intent(this, MainActivity.class);
-
         startActivity(main);
+
+        _loginButton.setEnabled(true);
+        _cadastroButton.setEnabled(true);
+    }
+
+    private boolean validaCampos() {
+        if(_cpfText.getText().toString().trim().equals(""))
+            return false;
+        else{
+            if (!ValidateCPFHelper.validaCPF(_cpfText.getText().toString()))
+                return false;
+        }
+        if(_senhaText.getText().toString().trim().equals(""))
+            return false;
+
+        return true;
     }
 
     private void pedirPermissoes() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
+                == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.INTERNET}, 2);
     }
+
 }
--- app/src/main/java/safewayapp/Activity/SignUpActivity.java
+++ app/src/main/java/safewayapp/Activity/SignUpActivity.java
@@ -5,14 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.InputMismatchException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import safewayapp.Helper.ValidateCPFHelper;
 import safewayapp.R;
 
 public class SignUpActivity extends AppCompatActivity {
     private static final int REQUEST_LOGIN = 0;
+
+    @BindView(R.id.txtNome)
+    EditText _nomeText;
+
+    @BindView(R.id.txtCpf)
+    EditText _cpfText;
+
+    @BindView(R.id.txtEmail)
+    EditText _emailText;
+
+    @BindView(R.id.txtSenha)
+    EditText _senhaText;
+
+    @BindView(R.id.btn_cadastro)
+    Button _cadastrarButton;
 
     @BindView(R.id.btn_voltar)
     Button _voltarButton;
@@ -23,14 +43,58 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
         ButterKnife.bind(this);
 
+        _cadastrarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cadastrar();
+            }
+        });
+
         _voltarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivityForResult(intent, REQUEST_LOGIN);
+                startActivity(intent);
                 finish();
             }
         });
-
     }
+
+    public void cadastrar() {
+        _cadastrarButton.setEnabled(false);
+        _voltarButton.setEnabled(false);
+
+        if(!validaCampos()){
+            _cadastrarButton.setEnabled(true);
+            _voltarButton.setEnabled(true);
+            Toast.makeText(getApplicationContext(), "Dados de preenchimento inválidos.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Lógica do cadastro de usuário
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+        _cadastrarButton.setEnabled(true);
+        _voltarButton.setEnabled(true);
+    }
+
+    private boolean validaCampos() {
+        if(_nomeText.getText().toString().trim().equals(""))
+            return false;
+        if(_cpfText.getText().toString().trim().equals(""))
+            return false;
+        else{
+            if (!ValidateCPFHelper.validaCPF(_cpfText.getText().toString()))
+                return false;
+        }
+        if(_emailText.getText().toString().trim().equals(""))
+            return false;
+        if(_senhaText.getText().toString().trim().equals(""))
+            return false;
+
+        return true;
+    }
+
 }
